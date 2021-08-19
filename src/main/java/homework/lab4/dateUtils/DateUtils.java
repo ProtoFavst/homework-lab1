f8da38df788d9506df3549c8e5e871dfab0c24ed
ProtoFavst <89038300+ProtoFavst@users.noreply.github.com>
2021-08-19 21:18:27 +0300

    Update DateUtils.java

--- src/main/java/homework/lab4/dateUtils/DateUtils.java
+++ src/main/java/homework/lab4/dateUtils/DateUtils.java
@@ -8,17 +8,14 @@ import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class DateUtils {
-    public void dateToString(Date date){
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy+MM+dd 'по времени' HH+mm+ss");
+   public void dateToString(LocalDateTime date){
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy+MM+dd 'по времени' HH+mm+ss");
         System.out.println(dateFormat.format(date));
 
     }
     public void stringTodate(String str){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy+MM+dd 'по времени' HH+mm+ss");
-        LocalDateTime ldt = LocalDateTime.parse(str, formatter);
-        ZonedDateTime zdt = ldt.atZone(ZoneId.systemDefault());
-        Date date = new Date(zdt.toInstant().toEpochMilli());
-        System.out.println(date);
+        System.out.println(LocalDateTime.parse(str, formatter));
 
     }
 
